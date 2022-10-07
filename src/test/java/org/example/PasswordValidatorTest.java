ce105ff8532f559671a3af56fa3143f842f6da20
Andy Trinh (UVM-STIL) <andt@netcompany.com>
2022-10-07 10:00:20 +0200

    Add tests to work with older maven

--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -1,25 +1,27 @@
 package org.example;
 
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class PasswordValidatorTest {
 
     @Test
     public void whenValidatingPassword_andItsTooShort_returnFalse() {
         var hasValidLength = PasswordValidator.hasValidLength("12345");
-        assert(hasValidLength == false);
+        assertFalse(hasValidLength);
     }
 
     @Test
     public void whenValidatingPassword_andItContainsNoSpecialCharacter_thenReturnFalse() {
-        var hasValidLength = PasswordValidator.hasSpecialCharacter("12345");
-        assert(hasValidLength == false);
+        var hasSpecialCharacter = PasswordValidator.hasSpecialCharacter("12345");
+        assertFalse(hasSpecialCharacter);
     }
 
     @Test
     public void whenValidatingPassword_andItContainsSpecialCharacter_thenReturnTrue() {
-        var hasValidLength = PasswordValidator.hasSpecialCharacter("12345%");
-        assert(hasValidLength == true);
+        var hasSpecialCharacter = PasswordValidator.hasSpecialCharacter("12345%");
+        assertTrue(hasSpecialCharacter);
     }
 }
